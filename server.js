2c2e80464c9331f3085c5a8cd35e3ed2caf4b549
Stanislav E. Govorov <mail@govorov.se.com>
2017-08-01 05:55:36 +0300

    minor changes for new rxjs workshop

--- server.js
+++ server.js
@@ -3,51 +3,80 @@
 const express = require('express');
 const cors    = require('cors');
 const _       = require('lodash');
+const morgan  = require('morgan');
+const delay   = require('express-delay');
 
-let app = express();
-
+const app = express();
 
 app.use(cors());
+app.use(morgan('[:date[clf]] :remote-addr :method :url :status [:response-time ms]'));
+app.use(delay(0, 1000));
 
 
 app.get('/',function(req,res){
-	res.send("Use /temperature, /precipation, /wind, /humidity");
+    res.send("Use /temperature, /precipation, /wind/speed, /wind/direction, /humidity");
 });
 
 
 app.get('/precipation',function(req,res){
-    let values = [
+    const values = [
         'clear',
-    	'rain',
-    	'hail',
-    	'snow',
-    	'lava',
+        'rain',
+        'hail',
+        'fog',
+        'snow',
+        'lightning',
+        'thunderstorm',
     ];
     res.json({result:_.sample(values)});
 });
 
 
-app.get('/wind',function(req,res){
-	let speed = Math.round(Math.random()*50);
+app.get('/wind/speed',function(req,res){
+    const speed = Math.round(Math.random()*30);
     res.json({result:speed});
 });
 
 
+app.get('/wind/direction',function(req,res){
+    const values = [
+        'n',
+        'nne',
+        'ne',
+        'ene',
+        'e',
+        'ese',
+        'se',
+        'sse',
+        's',
+        'ssw',
+        'sw',
+        'wsw',
+        'w',
+        'wnw',
+        'nw',
+        'nnw',
+    ];
+    res.json({result:_.sample(values)});
+});
+
+
 app.get('/temperature',function(req,res){
-	let min  = -40;
-	let max  =  40;
-	let temp = min + Math.round(Math.random()*(max-min));
-    // temp = Date.now();
+    const min  = 10;
+    const max  = 40;
+    const temp = min + Math.round(Math.random()*(max-min));
     res.json({result:temp});
 });
 
 
 app.get('/humidity',function(req,res){
-	let value = Math.round(Math.random()*95);
+    const min  = 15;
+    const max  = 95;
+    const value = min + Math.round(Math.random()*(max-min));
     res.json({result:value});
 });
 
-let port = process.env.PORT || 3000;
+const port = process.env.PORT || 8081;
 
 app.listen(port, function(){
     console.log(`App started on ${port}`);
